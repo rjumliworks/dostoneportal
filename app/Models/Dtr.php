1e0e2ef2716564978e49f4d34c40f461726ba1b5
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-04-07 12:28:00 +0800

    aw

--- app/Models/Dtr.php
+++ app/Models/Dtr.php
@@ -38,6 +38,11 @@ class Dtr extends Model
         return $this->belongsTo('App\Models\User', 'user_id', 'id');
     }
 
+    public function station()
+    {
+        return $this->belongsTo('App\Models\ListDropdown', 'station_id', 'id');
+    }
+
       public function getUpdatedAtAttribute($value)
     {
         return date('M d, Y g:i a', strtotime($value));
