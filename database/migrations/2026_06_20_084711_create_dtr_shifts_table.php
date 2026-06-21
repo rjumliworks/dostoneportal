75327b61c356b773062d6c3d86a65e98f1bec140
imkrad <164296287+imkrad@users.noreply.github.com>
2026-06-21 22:20:15 +0800

    aw

---
+++ database/migrations/2026_06_20_084711_create_dtr_shifts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dtr_shifts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dtr_shifts');
+    }
+};
