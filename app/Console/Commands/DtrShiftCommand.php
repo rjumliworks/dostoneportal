21329f95079bbe3a212ea53028bbb2fffa132e27
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-06-26 08:11:25 +0800

    Update DtrShiftCommand.php

--- app/Console/Commands/DtrShiftCommand.php
+++ app/Console/Commands/DtrShiftCommand.php
@@ -44,7 +44,7 @@ class DtrShiftCommand extends Command
                 $weekStart = $date->copy()->startOfWeek(); 
                 $weekEnd = $date->copy()->endOfWeek();     
 
-                $fridayHoliday = Schedule::where('event_id',31)->whereBetween('start', [$weekStart, $weekEnd])
+                $fridayHoliday = Schedule::where('event_id',1)->whereBetween('start', [$weekStart, $weekEnd])
                 ->whereRaw('DAYOFWEEK(start) = 6') // Friday (MySQL: 6 = Friday)
                 ->exists();
 
