621ffa6e07dc31cf7f91a2418aadba79cdcbdb99
imkrad <164296287+imkrad@users.noreply.github.com>
2026-07-19 17:52:53 +0800

    Update ExhibitorController.php

--- app/Http/Controllers/Api/ExhibitorController.php
+++ app/Http/Controllers/Api/ExhibitorController.php
@@ -10,6 +10,7 @@ use App\Models\ParticipantPoint;
 use Illuminate\Support\Facades\DB;
 use App\Models\EventExhibitorVisitor;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\Events\Session\FeedbackResource;
 
 class ExhibitorController extends Controller
 {
