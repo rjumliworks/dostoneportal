19ade0b00f7bd8206d811abe59380e6c7993420d
imkrad <164296287+imkrad@users.noreply.github.com>
2026-07-19 15:53:14 +0800

    Update FeedbackResource.php

--- app/Http/Resources/Api/Data/FeedbackResource.php
+++ app/Http/Resources/Api/Data/FeedbackResource.php
@@ -11,7 +11,7 @@ class FeedbackResource extends JsonResource
     {
         return [
             'rate' => $this->rate,
-            'id' => $this->feedbackable_id,
+            'session_id' => $this->feedbackable_id,
             'participant_id' => $this->participant->id,
             'comment' => $this->comment,
             'name' => $this->participant->firstname.' '.$this->participant->lastname,
