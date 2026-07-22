6fdc742c1576a8b17e051d925ae36ccd93ab0e94
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-07-22 17:52:46 +0800

    aw

--- resources/js/Pages/Modules/Events/Events/Components/Pages/Exhibitor.vue
+++ resources/js/Pages/Modules/Events/Events/Components/Pages/Exhibitor.vue
@@ -123,15 +123,25 @@ export default {
             .listen('SessionEvent', (event) => {
 
                 switch(event.type){
-                    case 'plus-ex':
-                        console.log(event.data);
-                        const index = this.exhibitors.findIndex(p => p.id === event.data.exhibitor_id);
-                        this.exhibitors[index].votes += 1;
-                    break;
-                    case 'minus-ex':
-                        const index2 = this.exhibitors.findIndex(p => p.id === event.data.exhibitor_id);
-                        this.exhibitors[index2].votes -= 1;
-                    break;
+                  case 'plus-ex':
+    const index = this.exhibitors.findIndex(
+        p => Number(p.id) === Number(event.data.exhibitor_id)
+    );
+
+    if (index !== -1) {
+        this.exhibitors[index].votes++;
+    }
+    break;
+
+case 'minus-ex':
+    const index2 = this.exhibitors.findIndex(
+        p => Number(p.id) === Number(event.data.exhibitor_id)
+    );
+
+    if (index2 !== -1) {
+        this.exhibitors[index2].votes--;
+    }
+    break;
                 }
             });
         },
