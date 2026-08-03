6e2f78127e1bb4cdd249896ec0fbb4b5332c9ab8
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-08-03 16:12:35 +0800

    Create 2026_08_03_161004_create_vips_table.php

---
+++ database/migrations/2026_08_03_161004_create_vips_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vips', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('name');
+            $table->text('designation');
+            $table->text('affiliation');
+            $table->string('face_id')->unique();
+            $table->string('image_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vips');
+    }
+};
