8718fee6278f78ef1a59b474fb8a3a2b8d57fac8
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-08-12 01:15:51 +0800

    aw

--- resources/views/certificates/appreciation.blade.php
+++ resources/views/certificates/appreciation.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Certificate</title>
+    <title>Certificate of Appreciation</title>
     <style>
         @font-face {
             font-family: 'Certificate Body';
