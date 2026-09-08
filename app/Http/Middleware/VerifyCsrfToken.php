854077ebe19ed49957432fbb2cd2ee88047d88fc
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-09-08 15:58:16 +0800

    aw

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-
+use Symfony\Component\HttpFoundation\Cookie;
 class VerifyCsrfToken extends Middleware
 {
     /**
@@ -14,4 +14,20 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    protected function newCookie($request, $config)
+    {
+        return new Cookie(
+            config('session.cookie') . '_xsrf',
+            $request->session()->token(),
+            $this->availableAt(60 * $config['lifetime']),
+            $config['path'],
+            $config['domain'],
+            $config['secure'],
+            false,
+            false,
+            $config['same_site'] ?? null,
+            $config['partitioned'] ?? false
+        );
+    }
 }
